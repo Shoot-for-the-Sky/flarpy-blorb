16874ff94073351a9edf119064ee33754ab18d8d
Kfir Goldfarb <kggold4@gmail.com>
2023-03-04 23:50:57 +0200

    add game over screen

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -6,20 +6,28 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrengh;
+    public LogicManager logic;
+    public bool birdAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrengh;
             // myRigidbody.transform.Rotate(now Vector2D())
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdAlive = false;
     }
 }
